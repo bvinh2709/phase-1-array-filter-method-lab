0755a1967a32a68c67b064f3d9e7e1f206941181
Vinh Nguyen <nguyenlebaovinh.27@gmail.com>
2023-02-06 09:20:36 -0600

    Completed array filter method lab

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(array, name) {
+    const driverList = array.filter(element => element.toLowerCase() === name.toLowerCase())
+    console.log(driverList)
+    return driverList
+}
+findMatching(drivers, "Bobby")
+
+
+function fuzzyMatch(array, name) {
+    const driverList = array.filter(element => element[0] + element[1] === name)
+    console.log(driverList)
+    return driverList
+}
+fuzzyMatch(drivers, "Sa")
+
+const driversObj = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+function matchName (array, name) {
+    const driverList = array.filter(element => element.name === "Bobby")
+    console.log(driverList)
+    return driverList
+}
+matchName(driversObj, "Bobby")
